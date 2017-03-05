fe1725dd7cbce5291db311ef835fcedb808827ba
CryHam <cryham@gmail.com>
2017-03-05 18:03:06 +0100

    List read from doublecmd.xml, bad colors

--- source/List.cpp
+++ source/List.cpp
@@ -19,14 +19,68 @@ void List::Default()
 //------------------------------------------------
 bool List::LoadFromDC(std::string file)
 {
-    Default();
+	XMLDocument doc;
+	XMLError er = doc.LoadFile(file.c_str());
+	if (er != XML_SUCCESS)
+	{	/*Can't load: "+file);*/  return false;  }
 
-    XMLDocument doc;
-    doc.LoadFile(file.c_str());
+	XMLElement* root = doc.RootElement();
+	if (!root)  return false;
+	string rn = root->Name();
+	if (rn != "doublecmd")  return false;
+	XMLElement* clrs = root->FirstChildElement("Colors");
+	if (!clrs)  return false;
+	XMLElement* filt = clrs->FirstChildElement("FileFilters");
+	if (!filt)  return false;
 
-    doc.RootElement();
+	Default();
 
-    return true;
+	///  load Filters
+	XMLElement* fi = filt->FirstChildElement("Filter");
+	if (!fi)  return false;
+
+	while (fi)
+	{
+		XMLElement* f,*c,*a;
+		Pat p;
+		f = fi->FirstChildElement("FileMasks");  p.s = f->GetText();
+		c = fi->FirstChildElement("Color");		 p.c = atol(c->GetText());
+		a = fi->FirstChildElement("Attributes"); //p.a =
+
+		//Clr cl;  cl.c = p.c;
+		clr.insert(p.c);
+
+		pat.push_back(p);
+		fi = fi->NextSiblingElement();
+	}
+/*
+	<doublecmd ..>
+	  <Configuration ..>
+	  <Colors>
+		<FileFilters>
+		  <Filter>
+			<Name>..</Name>
+			<FileMasks>*=.flac;*.d;</FileMasks>
+			<Color>10049581</Color>
+			<Attributes>**</Attributes>
+		  </Filter>
+*/
+	return true;
+}
+
+//  save, export to  DC doublecmd.xml
+//------------------------------------------------
+bool List::SaveToDC(std::string file)
+{
+	XMLDocument xml;
+	XMLElement* root = xml.NewElement("FileFilters");
+
+	XMLElement* ver = xml.NewElement("Filter");
+		ver->SetAttribute("num",	"12");
+	root->InsertEndChild(ver);
+
+	xml.InsertEndChild(root);
+	return xml.SaveFile(file.c_str());
 }
 
 //  load, import from  TC color.ini
--- source/List.h
+++ source/List.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <set>
+#include <SFML/Config.hpp>
 
 
 //  Pattern match, for e.g. extension or name
@@ -8,21 +10,24 @@
 struct Pat
 {
 	std::string s;  // eg. *.cpp
+	sf::Uint32 c;
 	bool dir, link;
 	Pat()
-		:dir(false), link(false)
+		:dir(false), link(false), c(0)
 	{   }
 };
 
-//  Color, for pattern
+//  Color, for patterns
 //------------------------------------------------
 struct Clr
 {
-	int c;
+	sf::Uint32 c;
 //	sf::Color clr;
 	Clr()
 		:c(0)
 	{	}
+	bool operator<(const Clr& o) const
+	{	return c < o.c;  }
 };
 
 //  List of both
@@ -31,13 +36,15 @@ class List
 {
 public:
 	std::vector<Pat> pat;  // patterns
-	std::vector<Clr> clr;  // colors
+	std::set<sf::Uint32> clr;  // colors
 //	std::map<Pat, Clr> pt2clr;
 	List();
 	void Default();
 
 	//  load, import
 	bool LoadFromDC(std::string file);  // DC doublecmd.xml
+	bool SaveToDC(std::string file);
+
 	bool ImportFromTC(std::string file);  // TC color.ini
 
 	//  project file, own
