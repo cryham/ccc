2178d71687cd93ae408914492ec2bec3df7439b7
CryHam <cryham@gmail.com>
2017-03-12 22:45:07 +0100

    split to AppDraw

--- source/App.h
+++ source/App.h
@@ -1,11 +1,10 @@
 #pragma once
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
+#include "AppDraw.h"
 #include "Settings.h"
 #include "List.h"
 
 
-class App
+class App : public AppDraw
 {
 public:
 	//  main
@@ -28,13 +27,18 @@ public:
 	void Resize(int x, int y), UpdSplit();
 
 
-	//  status
-	std::string txtStatus;  // status info
-	float hueStatus = 0.55f;
-	int iStatus = 0;
-	const int maxStatus = 60;  // 60=1sec
+	//  status info  -----
+	struct Status
+	{
+		std::string txt;
+		float hue = 0.55f;
+		int cnt = 0;
+		const int end = 80;  // 60 = 1sec
 
-	//  tabs
+		void Set(std::string sText, float hue);
+	}status;
+
+	//  tabs  -----
 	enum ETabs {
 		Tab_Edit, Tab_List, Tab_Settings, Tab_Help, Tab_ALL };
 	const char* tabNames[Tab_ALL] = {
@@ -45,9 +49,8 @@ public:
 
 	//  project
 	bool Load(), Save();
-	//  extras
-	void StartDC();
-	Settings set;
+	void StartDC();  // extras
+	Settings set;  //*  Settings
 
 
 	//  list operations
@@ -82,46 +85,10 @@ public:
 		Ed();
 	}ed;
 
-	int iCur =0;  // list cursor id,
-	void SetCur(int d), IncLine(int d);
+	int iCur =0;  // list cursor id
+	void SetCur(int d);
 
 	int line =0;  // page ofset lines
-	//int iPick =-1;  // mouse over cursor
-	List li;
-
-
-	//  sfml draw
-	//--------------------------
-	sf::RenderWindow* pWindow = nullptr;
-	sf::Sprite* pBackgr = nullptr;
-
-	sf::Font* pFont = nullptr;
-	sf::Text text;
-
-	sf::String str;
-	sf::Color clr;
-	bool bold = false;
-
-
-	//  set text color
-	//--------------------------
-	void Clr(sf::Uint8 r, sf::Uint8 g, sf::Uint8 b)
-	{
-		clr = sf::Color(r,g,b);
-	}
-	void Clr(const SClr& c)
-	{
-		clr = sf::Color(c.r, c.g, c.b);
-	}
-
-	//  write out text, from s
-	//  returns width, x advance
-	int Txt(int x, int y, bool draw=true);
-
-	//  clear rect
-	void Rect(int x, int y,  int sx, int sy,  const SClr& c);
-	void Rect(int x, int y,  int sx, int sy,  sf::Uint8 r, sf::Uint8 g, sf::Uint8 b);
-	//  frame rect
-	void Frame(int x, int y,  int sx, int sy,  int d,  const SClr& c);
-	void Frame(int x, int y,  int sx, int sy,  int d,  sf::Uint8 r, sf::Uint8 g, sf::Uint8 b);
+	void IncLine(int d);
+	List li;  //*  List
 };
---
+++ source/AppDraw.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "List.h"
+
+
+class AppDraw
+{
+public:
+	//  sfml draw
+	//--------------------------
+	sf::RenderWindow* pWindow = nullptr;
+	sf::Sprite* pBackgr = nullptr;
+
+	sf::Font* pFont = nullptr;
+	sf::Text text;
+
+	sf::String str;
+	sf::Color clr;
+	bool bold = false;
+
+
+	//  set text color
+	//--------------------------
+	void Clr(sf::Uint8 r, sf::Uint8 g, sf::Uint8 b)
+	{
+		clr = sf::Color(r,g,b);
+	}
+	void Clr(const SClr& c)
+	{
+		clr = sf::Color(c.r, c.g, c.b);
+	}
+
+	//  write out text, from s
+	//  returns width, x advance
+	int Txt(int x, int y, bool draw=true);
+
+	//  clear rect
+	void Rect(int x, int y,  int sx, int sy,  const SClr& c);
+	void Rect(int x, int y,  int sx, int sy,  sf::Uint8 r, sf::Uint8 g, sf::Uint8 b);
+	//  frame rect
+	void Frame(int x, int y,  int sx, int sy,  int d,  const SClr& c);
+	void Frame(int x, int y,  int sx, int sy,  int d,  sf::Uint8 r, sf::Uint8 g, sf::Uint8 b);
+};
